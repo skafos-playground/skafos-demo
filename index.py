187a660964f6ec50954e5547d6695feaffd2f432
Metis Machine's Skafos Playground <34212801+skafos-playground@users.noreply.github.com>
2017-12-12 13:24:09 -0500

    Add print statements, prettify

--- index.py
+++ index.py
@@ -2,41 +2,30 @@
 
 # coding: utf-8
 
-# ## Import the Skafos SDK
-
-# In[1]:
+## Import the Skafos SDK
 
 import os
 from skafossdk import *
 
-
-# ## Instantiate the Skafos SDK
-
-# In[2]:
+## Instantiate the Skafos SDK
 
 skafos = Skafos()
 
-
-# ## Create a Spark temp table from a Cassandra data source
-
-# In[3]:
+## Create a Spark temp table from a Cassandra data source
 
 skafos.engine.create_view(
     "weather_table", 
     {"keyspace": "weather", "table": "weather_noaa"}, 
     DataSourceType.Cassandra).result()
-
-
-# ## Select a subset of data from the Spark temp table
-
-# In[4]:
+print("Creating a spark temp view off of public weather data")
+      
+## Select a subset of data from the Spark temp table
 
 weather = skafos.engine.query("SELECT * from weather_table where zipcode = '23250' limit 3").result()
+print("Here's some of the weather data")
+print(weather)
 
-
-# ## Execute your weather prediction model that outputs data like this
-
-# In[5]:
+## Execute your weather prediction model that outputs data like this
 
 data = [
     {"date": "2017-11-29", "temp_avg": 53, "temp_min": 35, "temp_max": 71},
@@ -44,10 +33,10 @@ data = [
     {"date": "2017-12-01", "temp_avg": 52, "temp_min": 39, "temp_max": 65}
 ] 
 
+print("Here's my stubbed in weather prediction data")
+print(data)
 
-# ## Define schema of data to save
-
-# In[6]:
+## Define schema of data to save
 
 schema = {
     "table_name":
@@ -63,16 +52,11 @@ schema = {
     }
 }
 
+print("Here's the schema for my weather prediction data")
 
-# ## Save your model's results to your private keyspace
-
-# In[7]:
+## Save your model's results to your private keyspace
 
 dataresult = skafos.engine.save(schema, data).result()
 
-
-# In[ ]:
-
-
-
-
+print("Here's the result of my attempt to save my weather prediction data")
+print(dataresult)
